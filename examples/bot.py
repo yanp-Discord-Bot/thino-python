12457efaeac1d41f28c3d25a78082545c2fc5cbd
SawshaDev <sawshadev@pop-os.localdomain>
2022-05-12 17:02:58 -0700

    update bot.py

--- examples/bot.py
+++ examples/bot.py
@@ -4,12 +4,14 @@ import asyncio
 import thino
 
 bot = commands.Bot(command_prefix="prefixhere", intents=discord.Intents.default)
+client = thino.Client()
+
 
 @bot.command()
 async def nsfw(ctx: commands.Context):
-    r = await thino.img("tomboy")
+    r = await client.tomboy()
 
-    await ctx.send(r['url'])
+    await ctx.send(r.raw['url'])
 
 async def main():
     async with bot:
